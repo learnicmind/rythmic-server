b7798ccc23fbb5834375c1a76572b24b86770b76
Ariful Islam Razu <learnicmind@gmail.com>
2023-06-14 23:09:01 +0600

    delete method applied in manage users

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ async function run() {
       res.send(result);
     })
     
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
     })
 
     // here alluser = all users 
